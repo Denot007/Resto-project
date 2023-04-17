1122ac216e04490c3ea50708ace65dad64c50ff6
Denot007 <90220598+Denot007@users.noreply.github.com>
2023-04-17 02:27:03 -0400

    Commit with Media Quieries

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Hompage from "../../pages/Banner/Banner";
 
 const Main = () => {
-  return <main>
-    <Hompage/>
-  </main>;
+  return (
+    <main>
+      <Hompage />
+    </main>
+  );
 };
 
 export default Main;
